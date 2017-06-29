ecb2239f6a4b72d6c9665a05ff2d5813a5e7860e
Davide Principi <davide.principi@nethesis.it>
2017-06-29 09:33:06 +0200

    Fix PHP "String offset cast" notice

--- root/usr/share/nethesis/NethServer/Module/BackupConfig/Restore.php
+++ root/usr/share/nethesis/NethServer/Module/BackupConfig/Restore.php
@@ -99,6 +99,6 @@ class Restore extends \Nethgui\Controller\Table\RowAbstractAction
     function getHumanFilesize($bytes, $decimals = 2) {
         $sz = 'BKMGTP';
         $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$decimals}f %sB", $bytes / pow(1024, $factor), @$sz[$factor]);
+        return sprintf("%.{$decimals}f %sB", $bytes / pow(1024, $factor), @$sz[intval($factor)]);
     }
 }
